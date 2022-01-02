625c773863f5e75fc946474cb5d2b0d6eb17aa6a
Rafael Brusiquesi Martins <59908809+THE-RAF@users.noreply.github.com>
2022-01-02 19:22:30 -0300

    Add files via upload

---
+++ Basic Genetic Algorithm/genetic_algorithm_3D_version/backend/GA/genomes.py
@@ -0,0 +1,72 @@
+import numpy as np
+import random
+from copy import deepcopy
+
+
+class BaseGenome:
+    def __init__(self, n_chromossomes, chromossome_size=None, set_genes=None):
+        if not set_genes:
+            self.chromossomes = [str().join([random.choice(['0', '1']) for _ in range(chromossome_size)]) for _ in range(n_chromossomes)]
+
+        else:
+            self.genes = set_genes
+
+    def evaluate(self, fitness_function):
+        return fitness_function(self)
+
+    @staticmethod
+    def mutate_chromossome(chromossome):
+        random_index = np.random.randint(0, len(chromossome))
+        new_bit = '1' if chromossome[random_index] == '0' else '0'
+        new_chromossome = chromossome[:random_index] + new_bit + chromossome[random_index+1:]
+
+        return new_chromossome
+
+    @staticmethod
+    def chromossome_crossover(chromossome1, chromossome2):
+        cutting_index = np.random.randint(1, len(chromossome1)-1)
+        g1_1st_half, g1_2nd_half = chromossome1[:cutting_index], chromossome1[cutting_index:]
+        g2_1st_half, g2_2nd_half = chromossome2[:cutting_index], chromossome2[cutting_index:]
+
+        new_chromossome1 = g1_1st_half + g2_2nd_half
+        new_chromossome2 = g2_1st_half + g1_2nd_half
+
+        return new_chromossome1, new_chromossome2
+
+    @staticmethod
+    def mutate_genome(genome):
+        new_genome = deepcopy(genome)
+        for i, chromossome in enumerate(genome.chromossomes):
+            new_genome.chromossomes[i] = BaseGenome.mutate_chromossome(chromossome)
+
+        return new_genome
+
+    @staticmethod
+    def crossover_genome(genome1, genome2):
+        new_genome1 = deepcopy(genome1)
+        new_genome2 = deepcopy(genome2)
+
+        for i, (chromossome1, chromossome2) in enumerate(zip(genome1.chromossomes, genome2.chromossomes)):
+            new_chromossome1, new_chromossome2 = BaseGenome.chromossome_crossover(chromossome1, chromossome2)
+            new_genome1.chromossomes[i] = new_chromossome1
+            new_genome2.chromossomes[i] = new_chromossome2
+
+        return new_genome1, new_genome2
+
+    def __len__(self):
+        return len(self.genes)
+
+    def __str__(self):
+        return str().join([chromossome + '\n' for chromossome in self.chromossomes])
+
+
+def create_genome_class(n_chromossomes, chromossome_size):
+    class Genome(BaseGenome):
+        def __init__(self):
+            BaseGenome.__init__(self,
+                                n_chromossomes=n_chromossomes,
+                                chromossome_size=chromossome_size,
+                                set_genes=None
+                                )
+
+    return Genome
---
+++ Basic Genetic Algorithm/genetic_algorithm_3D_version/backend/GA_plotting_handler.py
@@ -0,0 +1,157 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from backend.GA.genetic_algorithm import GeneticAlgorithm
+from backend.GA.genomes import create_genome_class
+from backend.helpers import translate_bits_to_float
+
+
+class GALivePlotHandler1D:
+    def __init__(self,
+                 equation_string,
+                 interval,
+                 parameters,
+                 ):
+
+        self.equation_string = equation_string
+        self.interval = interval
+
+        self.GA = GeneticAlgorithm(
+            fitness_function=lambda genome: self.fitness_function(genome, self.equation_string),
+            genome_type=create_genome_class(n_chromossomes=1, chromossome_size=10),
+            parameters=parameters,
+            )
+
+    def fitness_function(self, genome, equation_string):
+        x = translate_bits_to_float(bit_string=genome.chromossomes[0][::-1], interval=self.interval)
+
+        return eval(equation_string)
+
+    def get_xy_values_by_population(self, population):
+        xs = []
+        ys = []
+
+        for genome in population:
+            xs.append(translate_bits_to_float(bit_string=genome.chromossomes[0][::-1], interval=self.interval))
+            ys.append(self.GA.fitness_function(genome))
+
+        return xs, ys
+
+    def get_best_xy_values(self):
+        best_genome = self.GA.best_genome
+        x = translate_bits_to_float(bit_string=best_genome.chromossomes[0][::-1], interval=self.interval)
+        y = self.GA.fitness_function(best_genome)
+
+        return x, y
+
+    def plot_GA(self,
+                figure,
+                canvas,
+                timer,
+                ):
+
+        x_equation = np.linspace(self.interval[0], self.interval[1], num=300)
+        y_equation = [eval(f'lambda x: {self.equation_string}')(x) for x in x_equation]
+
+        current_population, done = self.GA.run_generation()
+        x_genomes, y_genomes = self.get_xy_values_by_population(current_population)
+
+        best_genome = self.GA.best_genome
+        best_genome_x, best_genome_y = self.get_xy_values_by_population([best_genome])
+
+        figure.clear()
+
+        ax_GA = figure.add_subplot(121)
+        ax_GA.plot(x_equation, y_equation, c='k', alpha=0.7, zorder=0)
+        ax_GA.scatter(x_genomes, y_genomes, c='#f17587', s=10, zorder=1)
+        ax_GA.scatter(best_genome_x, best_genome_y, c='#77cdf2', s=15, zorder=1)
+
+        ax_performance = figure.add_subplot(122)
+        ax_performance.plot(self.GA.best_fitnesses, c='#77cdf2')
+        ax_performance.plot(self.GA.mean_fitnesses, c='#f17587')
+
+        plt.tight_layout()
+        canvas.draw()
+
+        if done: timer.stop()
+
+
+class GALivePlotHandler2D:
+    def __init__(self,
+                 equation_string,
+                 intervals,
+                 parameters,
+                 ):
+        self.equation_string = equation_string
+        self.intervals = intervals
+
+        self.GA = GeneticAlgorithm(
+            fitness_function=lambda genome: self.fitness_function(genome, self.equation_string),
+            genome_type=create_genome_class(n_chromossomes=2, chromossome_size=parameters['chromossome_size']),
+            parameters=parameters,
+        )
+
+    def fitness_function(self, genome, equation_string):
+        x = translate_bits_to_float(bit_string=genome.chromossomes[0][::-1], interval=self.intervals[0])
+        y = translate_bits_to_float(bit_string=genome.chromossomes[1][::-1], interval=self.intervals[1])
+
+        return eval(equation_string)
+
+    def get_xyz_values_by_population(self, population):
+        xs = []
+        ys = []
+        zs = []
+
+        for genome in population:
+            xs.append(translate_bits_to_float(bit_string=genome.chromossomes[0][::-1], interval=self.intervals[0]))
+            ys.append(translate_bits_to_float(bit_string=genome.chromossomes[1][::-1], interval=self.intervals[1]))
+            zs.append(self.GA.fitness_function(genome))
+
+        return xs, ys, zs
+
+    def get_best_xyz_values(self):
+        best_genome = self.GA.best_genome
+        x = translate_bits_to_float(bit_string=best_genome.chromossomes[0][::-1], interval=self.intervals[0])
+        y = translate_bits_to_float(bit_string=best_genome.chromossomes[1][::-1], interval=self.intervals[1])
+        z = self.GA.fitness_function(best_genome)
+
+        return x, y, z
+
+    def plot_GA(self,
+                figure,
+                canvas,
+                timer,
+                ):
+
+        x = np.arange(self.intervals[0][0], self.intervals[0][1], 0.1)
+        y = np.arange(self.intervals[1][0], self.intervals[1][1], 0.1)
+
+        x, y = np.meshgrid(x, y)
+        z = eval(self.equation_string)
+
+        current_population, done = self.GA.run_generation()
+        x_genomes, y_genomes, z_genomes = self.get_xyz_values_by_population(current_population)
+
+        best_genome = self.GA.best_genome
+        best_genome_x, best_genome_y, best_genome_z = self.get_xyz_values_by_population([best_genome])
+
+        figure.clear()
+
+        ax_GA = figure.add_subplot(121, projection='3d', zorder=0)
+        ax_GA.plot_wireframe(x, y, z, color='k', zorder=0, alpha=0.3)
+
+        red_hexcode = '#f4404c'
+        blue_hexcode = '#0000ff'
+        light_blue_hexcode = '#77cdf2'
+
+        ax_GA.scatter(x_genomes, y_genomes, z_genomes, c=red_hexcode, s=10, zorder=0)
+        ax_GA.scatter(best_genome_x, best_genome_y, best_genome_z, c=blue_hexcode, s=15, zorder=1)
+
+        ax_performance = figure.add_subplot(122)
+        ax_performance.plot(self.GA.best_fitnesses, c=light_blue_hexcode)
+        ax_performance.plot(self.GA.mean_fitnesses, c=red_hexcode)
+
+        plt.tight_layout()
+        canvas.draw()
+
+        if done: timer.stop()
